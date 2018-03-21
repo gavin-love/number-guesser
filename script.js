0cafbdeaf5380e19e27ab0cf0cb1faad042d2e1e
gavin-love <1co.loco@oprotonmail.ch>
2018-03-20 18:35:57 -0600

    Add create your own adventure function

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 var formInput = document.querySelector(".input");
+var cyoaInputs = document.querySelectorAll(".cyoa-inputs")
+
 var submitBtn = document.querySelector(".guess-button");
 var clearBtn = document.querySelector(".clear-button");
 var resetBtn = document.querySelector(".reset-button");
-var enableBtns = document.querySelectorAll(".enable-buttons")
+var enableBtns = document.querySelectorAll(".enable-buttons");
+var cyoaBtn = document.querySelector(".cyoa-button");
+
+var hiddenDisplay = document.querySelectorAll(".hidden-display")
+
 var belowRange = 0;
 var aboveRange = 101;
 var min = 1;
 var max = 100;
-var placeholderMin = -9
-var placeholderMax = 110
+var placeholderMin = -9;
+var placeholderMax = 110;
+var wins = 0;
+
 var randomNumber = Math.floor(Math.random() * (max - min) + min);
 console.log(randomNumber);
 
@@ -16,6 +24,7 @@ submitBtn.addEventListener('click', getNumber);
 resetBtn.addEventListener('click', resetGame);
 clearBtn.addEventListener('click', clearInput);
 formInput.addEventListener('keyup', enableButtons);
+cyoaBtn.addEventListener('click', customRangeFunction);
 
 function  getNumber() {
   var displayNmbr = document.querySelector(".number-display");
@@ -71,8 +80,42 @@ function levelUp() {
   aboveRange = aboveRange+10;
   min = min-10;
   max = max+10;
+  wins++;
+  if (wins < 1) {
+    generateRandomNumber(min,max)
+  } else {
+    enableCustomGame()
+  }
+};
 
-  generateRandomNumber(min,max)
+function enableCustomGame() {
+  var lastGuessText = document.querySelector(".last-guess-text")
+
+  for (var i = 0; i < hiddenDisplay.length; i++) {
+    hiddenDisplay[i].style = "display: visible";
+    cyoaBtn.disabled = false;
+    lastGuessText.innerHTML = "Your last guess was"
+    formInput.placeholder = ''
+  }
+};
+
+function customRangeFunction() {
+  var guessDetails = document.querySelector(".guess-details-display");
+  var lastGuessText = document.querySelector(".last-guess-text")
+
+  for (var i = 0; i < cyoaInputs.length; i++) {
+    var min = cyoaInputs[0].value;
+    var max = cyoaInputs[1].value;
+    placeholderMin = min;
+    placeholderMax = max;
+    belowRange = min;
+    aboveRange = max;
+  };
+    lastGuessText.innerHTML = "the range is now " + placeholderMin + " to " + placeholderMax;
+    guessDetails.innerHTML = "WINNER!";
+    formInput.placeholder = "Guess a number between " + placeholderMin + " and " + placeholderMax;
+
+  generateRandomNumber(min,max);
 };
 
 function generateRandomNumber(min,max) {
@@ -100,7 +143,7 @@ function clearInput() {
   var lastGuessText = document.querySelector(".last-guess-text")
   var guessDetails = document.querySelector(".guess-details-display");
 
-  lastGuessText.innerHTML = "Your last guess was"
+  // lastGuessText.innerHTML = "Your last guess was"
   formInput.value = '';
   formInput.focus();
   disableButtons();
